dd594dfb6984055b7ff6f3295bb5d13bfb33cb1d
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2025-08-01 19:40:00 +0530

    richtext editor

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,7 +12,15 @@ export function formatCurrency(value: number | string) {
     maximumFractionDigits: 0,
   }).format(Number(value));
 };
+export function generateTenantURL(tenantSlug: string) {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === "true";
 
-export function generateTenantURL(slug: string) {
-  return `/tenants/${slug}`;
-}
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
+    return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+  }
+
+  const protocol = "https";
+  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+  return `${protocol}://${tenantSlug}.${domain}`;
+};
--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -8,6 +8,7 @@ import { TRPCError } from "@trpc/server";
 import Stripe from "stripe";
 import { CheckoutMetadata, ProductMetadata } from "../types";
 import { stripe } from "@/lib/stripe";
+import { generateTenantURL } from "@/lib/utils";
 
 export const checkoutRouter = createTRPCRouter({
     purchase: protectedProcedure
@@ -82,11 +83,11 @@ export const checkoutRouter = createTRPCRouter({
             }
           }
         }));
-
+        const domain = generateTenantURL(input.tenantSlug);
         const checkout = await stripe.checkout.sessions.create({
         customer_email: ctx.session.user.email,
-        success_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?success=true`,
-        cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?cancel=true`,
+        success_url: `${domain}/checkout?success=true`,
+        cancel_url: `${domain}/checkout?cancel=true`,
         mode: "payment",
         line_items: lineItems,
         invoice_creation: {
